fb3052e9573438e063546dbeb52c610597b3a438
AdriaRos <adria.ros@elma.care>
2021-12-29 16:13:41 +0100

    main
    - Initial commit

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Adrià Ros on 29/12/21.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
