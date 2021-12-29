30370e44f2f4fd494961e345ae03fef72a04696c
AdriaRos <adria.ros@elma.care>
2021-12-29 17:30:58 +0100

    main
    - Load a random word from txt

--- WordScramble/WordScramble/ContentView.swift
+++ WordScramble/WordScramble/ContentView.swift
@@ -17,7 +17,7 @@ struct ContentView: View {
         
         NavigationView {
             
-            List() {
+            List {
                 
                 Section {
                     TextField("Enter your word", text: $newWord)
@@ -34,8 +34,22 @@ struct ContentView: View {
                     }
                 }
             }
+            
+            .navigationTitle(rootWord)
+            .onAppear(perform: startGame)
         }
-        .navigationTitle(rootWord)
+    }
+    
+    func startGame() {
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            if let startWords = try? String(contentsOf: startWordsURL) {
+                let allWords = startWords.components(separatedBy: "\n")
+                rootWord = allWords.randomElement() ?? "silkworm"
+                return
+            }
+        }
+        
+        fatalError("Could not load start.text from Bundle")
     }
     
     func addNewWord() {
